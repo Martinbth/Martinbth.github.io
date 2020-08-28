ff2315ef8a95150a1ecb9a47465fb24a216644ad
Martin Bååth <id13mbh@cs.umu.se>
2020-08-28 16:29:42 +0200

    add profile pic

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,25 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {
-      colors: {
-        primary:'#FF6363',
-        secondary:{
-          100:'#E2E2D5',
-          200:'#888883',
-        }
-      }
-    },
-    fontFamily:{
+    fontFamily: {
       body: ['Noto Sans JP']
-    }
+    },
+    extend: {
+      fontSize: {
+        '7xl': '7rem',
+        '8xl': '8rem',
+        '9xl': '9em',
+        '10xl': '10rem',
+
+      },
+      colors: {
+        primary: '#FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883',
+        }
+      },
+    },
   },
   variants: {},
   plugins: [],
