54630393fbca5796649788153c84baf01449279d
Martin Bååth <id13mbh@cs.umu.se>
2020-09-07 10:19:39 +0200

    add pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 module.exports = {
   purge: [],
   theme: {
-    fontFamily: {
-      body: ['Noto Sans JP']
-    },
+
     extend: {
+      animation: {
+        fadeIn: "fadeIn 0.3s ease-in forwards"
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 }
+        }
+      },
       fontSize: {
         '7xl': '7rem',
         '8xl': '8rem',
@@ -17,10 +24,17 @@ module.exports = {
         secondary: {
           100: '#E2E2D5',
           200: '#888883',
+         300:'#1a1a1b'
         }
       },
+      scale: {
+        '101': '1.01',
+        '102': '1.02',
+      }
     },
   },
-  variants: {},
+  variants: {
+     animation: ["motion-safe"]
+  },
   plugins: [],
 }
